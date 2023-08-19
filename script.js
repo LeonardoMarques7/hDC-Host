77214b24cfcb98531755a0cb23d74aa5c59b22ee
Leonardo <leonardoemcs155@gmail.com>
2023-08-19 18:16:47 -0300

    page login - in javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+const users = [
+    { nome: 'leléo', email: 'leleo@gmail.com', senha: '752895'},
+    { nome: 'dandan', email: 'dandan@gmail.com', senha: '752895'}
+];
+
+function AbrirSite () {
+    window.location.href = 'index.html';
+}
+
+function validarUser() { 
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const user = users.find(user => user.email === email && user.senha === senha); 
+
+    if (user) { 
+        alert(`Bem-vindo ${user.nome}!`);
+        AbrirSite();
+    } 
+    else 
+    alert('Usuário não encontrado!!');
+}
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+        validarUser();
+});
