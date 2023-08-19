8cb40d0995872a0a1b0c88e6620dc8c4028dde65
Leonardo <leonardoemcs155@gmail.com>
2023-08-19 18:30:19 -0300

    Teste login

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const users = [
-    { nome: 'leléo', email: 'leleo@gmail.com', senha: '752895'},
-    { nome: 'dandan', email: 'dandan@gmail.com', senha: '752895'}
+    { nome: 'leléo', email: 'leleo@gmail.com', senha: 752895},
+    { nome: 'dandan', email: 'dandan@gmail.com', senha: 752895}
 ];
 
 function AbrirSite () {
@@ -11,7 +11,9 @@ function validarUser() {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    const user = users.find(user => user.email === email && user.senha === senha); 
+    const senhaNum = parseInt(senha);
+
+    const user = users.find(user => user.email === email && user.senha === senhaNum); 
 
     if (user) { 
         alert(`Bem-vindo ${user.nome}!`);
